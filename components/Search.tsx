988b009b283f05f62f0437d91fb730a6ebc35392
Milena Kukolj <mkukolj@protonmail.com>
2023-05-23 22:17:58 +0200

    Add missing types in Search

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,15 +1,11 @@
 'use client'
 import { Center, Flex, Input, Spacer } from "@chakra-ui/react"
 import { SearchIcon } from '@chakra-ui/icons'
-import { useContext, useEffect, useRef } from "react";
+import { FormEvent, useContext, useEffect, useRef } from "react";
 import { ISearchContext, SearchContext } from "@/app/providers";
 
 export default function Search() {
     const filter = useContext<ISearchContext>(SearchContext)
-
-    const setFilter = (e) => {
-        filter.setSearchFilter(e.target.value)
-    }
 
     return (
         <Flex border='1px' borderColor='gray.200' borderRadius='lg' _focus={{ border: '1px solid blue' }}>
@@ -20,7 +16,7 @@ export default function Search() {
                     boxSize={6}
                 />
             </Center>
-            <Input onChange={setFilter} variant='flushed' colorScheme="white" textColor='black' placeholder="Search" size='lg' />
+            <Input onChange={(e: FormEvent<HTMLInputElement>) => filter.setSearchFilter(e.currentTarget.value)} variant='flushed' colorScheme="white" textColor='black' placeholder="Search" size='lg' />
         </Flex>
     );
 }
